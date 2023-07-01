f7152606f954229eb9a7e5711317b4fcf5c3679a
Planethac <info@planethac.de>
2023-07-01 11:08:42 +0200

    deleted:    BUILD/make-shell.bat 	deleted:    BUILD/shell.exe 	new file:   BUILD/shell/make-shell.bat 	new file:   BUILD/shell/shell.exe 	modified:   SRC/K/SYSTEM/SHELL/SHELL-COMMAND.CS 	modified:   SRC/K/SYSTEM/SHELL/SHELL-FUNCTIONS.CS 	modified:   SRC/K/SYSTEM/SHELL/SHELL-MAIN.CS

--- BUILD/make-shell.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-csc /out:shell.exe /t:exe /nologo ..\src\k\system\shell\*.cs
-@echo on
---
+++ BUILD/shell/make-shell.bat
@@ -0,0 +1,3 @@
+@echo off
+csc /out:shell.exe /t:exe /nologo ..\..\src\k\system\shell\*.cs
+@echo on
