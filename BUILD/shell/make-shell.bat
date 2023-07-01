b0915d451bdc458df0aa62c0834c83ebf5ae4b6f
Planethac <info@planethac.de>
2023-07-01 16:13:02 +0200

    modified:   BUILD/shell/make-shell.bat 	new file:   BUILD/shell/run.bat 	deleted:    BUILD/shell/shell.exe 	new file:   SRC/K/SYSTEM/GUI/App.xaml 	new file:   SRC/K/SYSTEM/GUI/App.xaml.cs 	new file:   SRC/K/SYSTEM/GUI/AssemblyInfo.cs 	new file:   SRC/K/SYSTEM/GUI/GUI.csproj 	new file:   SRC/K/SYSTEM/GUI/GUI.deps.json 	new file:   SRC/K/SYSTEM/GUI/GUI.dll 	new file:   SRC/K/SYSTEM/GUI/GUI.exe 	new file:   SRC/K/SYSTEM/GUI/GUI.pdb 	new file:   SRC/K/SYSTEM/GUI/GUI.runtimeconfig.json 	new file:   SRC/K/SYSTEM/GUI/MainWindow.xaml 	new file:   SRC/K/SYSTEM/GUI/MainWindow.xaml.cs 	new file:   SRC/K/SYSTEM/GUI/bin/Debug/net7.0-windows/GUI.deps.json 	new file:   SRC/K/SYSTEM/GUI/bin/Debug/net7.0-windows/GUI.dll 	new file:   SRC/K/SYSTEM/GUI/bin/Debug/net7.0-windows/GUI.exe 	new file:   SRC/K/SYSTEM/GUI/bin/Debug/net7.0-windows/GUI.pdb 	new file:   SRC/K/SYSTEM/GUI/bin/Debug/net7.0-windows/GUI.runtimeconfig.json 	new file:   SRC/K/SYSTEM/GUI/obj/Debug/net7.0-windows/.NETCoreApp,Version=v7.0.AssemblyAttributes.cs 	new file:   SRC/K/SYSTEM/GUI/obj/Debug/net7.0-windows/App.g.cs 	new file:   SRC/K/SYSTEM/GUI/obj/Debug/net7.0-windows/GUI.AssemblyInfo.cs 	new file:   SRC/K/SYSTEM/GUI/obj/Debug/net7.0-windows/GUI.AssemblyInfoInputs.cache 	new file:   SRC/K/SYSTEM/GUI/obj/Debug/net7.0-windows/GUI.GeneratedMSBuildEditorConfig.editorconfig 	new file:   SRC/K/SYSTEM/GUI/obj/Debug/net7.0-windows/GUI.assets.cache 	new file:   SRC/K/SYSTEM/GUI/obj/Debug/net7.0-windows/GUI.csproj.AssemblyReference.cache 	new file:   SRC/K/SYSTEM/GUI/obj/Debug/net7.0-windows/GUI.csproj.CoreCompileInputs.cache 	new file:   SRC/K/SYSTEM/GUI/obj/Debug/net7.0-windows/GUI.csproj.FileListAbsolute.txt 	new file:   SRC/K/SYSTEM/GUI/obj/Debug/net7.0-windows/GUI.dll 	new file:   SRC/K/SYSTEM/GUI/obj/Debug/net7.0-windows/GUI.g.resources 	new file:   SRC/K/SYSTEM/GUI/obj/Debug/net7.0-windows/GUI.genruntimeconfig.cache 	new file:   SRC/K/SYSTEM/GUI/obj/Debug/net7.0-windows/GUI.pdb 	new file:   SRC/K/SYSTEM/GUI/obj/Debug/net7.0-windows/GUI_MarkupCompile.cache 	new file:   SRC/K/SYSTEM/GUI/obj/Debug/net7.0-windows/GUI_MarkupCompile.lref 	new file:   SRC/K/SYSTEM/GUI/obj/Debug/net7.0-windows/GUI_inml3nuv_wpftmp.AssemblyInfo.cs 	new file:   SRC/K/SYSTEM/GUI/obj/Debug/net7.0-windows/GUI_inml3nuv_wpftmp.AssemblyInfoInputs.cache 	new file:   SRC/K/SYSTEM/GUI/obj/Debug/net7.0-windows/GUI_inml3nuv_wpftmp.GeneratedMSBuildEditorConfig.editorconfig 	new file:   SRC/K/SYSTEM/GUI/obj/Debug/net7.0-windows/GUI_inml3nuv_wpftmp.assets.cache 	new file:   SRC/K/SYSTEM/GUI/obj/Debug/net7.0-windows/MainWindow.baml 	new file:   SRC/K/SYSTEM/GUI/obj/Debug/net7.0-windows/MainWindow.g.cs 	new file:   SRC/K/SYSTEM/GUI/obj/Debug/net7.0-windows/apphost.exe 	new file:   SRC/K/SYSTEM/GUI/obj/Debug/net7.0-windows/ref/GUI.dll 	new file:   SRC/K/SYSTEM/GUI/obj/Debug/net7.0-windows/refint/GUI.dll 	new file:   SRC/K/SYSTEM/GUI/obj/GUI.csproj.nuget.dgspec.json 	new file:   SRC/K/SYSTEM/GUI/obj/GUI.csproj.nuget.g.props 	new file:   SRC/K/SYSTEM/GUI/obj/GUI.csproj.nuget.g.targets 	new file:   SRC/K/SYSTEM/GUI/obj/project.assets.json 	new file:   SRC/K/SYSTEM/GUI/obj/project.nuget.cache 	modified:   SRC/K/SYSTEM/SHELL/Commands.CS 	modified:   SRC/K/SYSTEM/SHELL/File.CS 	modified:   SRC/K/SYSTEM/SHELL/Functions.CS 	modified:   SRC/K/SYSTEM/SHELL/Main.CS 	new file:   SRC/K/SYSTEM/SHELL/shell.exe

--- BUILD/shell/make-shell.bat
+++ BUILD/shell/make-shell.bat
@@ -1,5 +1,8 @@
 @echo off
 echo Compiling ...
-csc /out:shell.exe /t:exe /nologo ..\..\src\k\system\shell\*.cs
+csc /out:..\..\src\k\system\shell\shell.exe /t:exe /nologo ..\..\src\k\system\shell\*.cs
+cd ..\..\src\k\system\gui
+dotnet build
+cp -r bin\Debug\net7.0-windows\* .
 echo Succes!
 @echo on
