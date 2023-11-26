55f30d3334127e83df11c373dabd791b727fd6bb
Planethac <info@planethac.de>
2023-11-26 20:35:49 +0100

    bug fixes

--- BUILD/shell/make-shell.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Compiling ...
-csc /out:..\..\src\k\system\shell\shell.exe /t:exe /nologo ..\..\src\k\system\shell\*.cs
-cd ..\..\src\k\system\gui
-msbuild.exe
-copy -r bin\Debug\net7.0-windows\* .
-echo Succes!
-@echo on
--- BUILD/shell/run.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd ..\..\src\k\system\shell\
-shell.exe
